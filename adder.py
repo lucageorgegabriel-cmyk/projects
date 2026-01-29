449c3f0fd3e49ff4b4647d22428f0fa0c03388fc
George Luca <george.luca@igwpower.com>
2026-01-29 16:19:36 +0200

    Add simple adder project

---
+++ adder.py
@@ -0,0 +1,16 @@
+def main():
+    print("Simple Adder 🧮")
+
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+
+    a = float(a)
+    b = float(b)
+
+    result = a + b
+
+    print(f"Result: {a} + {b} = {result}")
+
+
+if __name__ == "__main__":
+    main()
